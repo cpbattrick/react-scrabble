01f4c205f8c2f069d705488ef511d0941854be76
cpbattrick <cpbattrick@googlemail.com>
2019-07-31 07:57:56 -0400

    changed controls component to functional

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,44 +1,33 @@
 import React from "react";
 import axios from "axios";
 
-class Controls extends React.Component {
-  state = {
-    response: "",
-    revealAnswer: false
-  };
-
-  checkAnswer = e => {
-    let answer = this.props.upperArray.join("");
+const Controls = props => {
+  const checkAnswer = e => {
+    let answer = props.upperArray.join("");
     if (answer.length < 5) {
-      this.props.setResponse("Try Again!");
+      props.setResponse("Try Again!");
     } else {
       axios
         .get(`http://tom.mo2.dinksurveys.net/api/1.0/isword?word=${answer}`)
         .then(({ data }) => {
           if (!data.isWord) {
-            this.props.setResponse("Try Again!");
+            props.setResponse("Try Again!");
           } else {
-            this.props.setResponse("Well Done!");
+            props.setResponse("Well Done!");
           }
         });
     }
   };
 
-  exposeAnswer = () => {
-    this.props.showAnswer();
-  };
-
-  render() {
-    return (
-      <div id="controlsContainer">
-        <div id="answer">{this.props.revealAnswer && this.props.word}</div>
-        <div id="response">{this.props.response}</div>
-        <button onClick={this.props.newWord}>New Word</button>
-        <button onClick={this.checkAnswer}>Check Answer</button>
-        <button onClick={this.props.showAnswer}>Reveal Word</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div id="controlsContainer">
+      <div id="answer">{props.revealAnswer && props.word}</div>
+      <div id="response">{props.response}</div>
+      <button onClick={props.newWord}>New Word</button>
+      <button onClick={checkAnswer}>Check Answer</button>
+      <button onClick={props.showAnswer}>Reveal Word</button>
+    </div>
+  );
+};
 
 export default Controls;
